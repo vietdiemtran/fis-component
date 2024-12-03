ad9e9f8bc7eb61364a00b3e808786a65ff2337ac
DiemTV2 <vietdiemtran@gmail.com>
2024-12-03 17:26:51 +0700

    update

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,7 @@
 import { Button } from "antd";
 
 const FisButton = () => {
-  return <Button>Hello from my React component!3</Button>;
+  return <Button>Hello from my React component!4</Button>;
 };
 
 export default FisButton;

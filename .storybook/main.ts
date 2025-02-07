5a79a681d0d79bbb21a0c9d0892e0a39a5348461
DiemTV2 <vietdiemtran@gmail.com>
2025-02-07 16:45:29 +0700

    update version

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,24 +2,31 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+
+  docs: {},
+
   core: {
     builder: "@storybook/builder-vite",
   },
+
   viteFinal: (config) => {
     return config;
   },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;

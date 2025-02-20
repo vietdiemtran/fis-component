18a5ef0cfe11022c36101c2066a1323941a82498
DiemTV2 <vietdiemtran@gmail.com>
2025-02-20 16:47:06 +0700

    build

--- src/components/Button/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "antd";
-
-const FisButton = () => {
-  return <Button>Hello from my React component!4</Button>;
-};
-
-export default FisButton;
--- src/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as FisButton } from "./components/Button";
